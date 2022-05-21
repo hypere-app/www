29d1aa97422bce415950401c17953ec1c1b0221f
Adi Stroianu <astroianu530@gmail.com>
2022-05-21 17:24:18 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
 import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
 import "./App.css";
 
 class App extends React.Component {
@@ -13,28 +11,21 @@ class App extends React.Component {
             <a href=".">LOGO</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+
+            <a href="https://twitter.com/astroianu" title="Follow for updates!" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
+
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>The internet. Changed forever.</h1>
+            <p>Hypere Browser is the future of web brosing. The first web browser that actually listens to it's user's requests. Press the button down below to shoot us an email with your feature request.</p>
           </div>
-          <a href="mailto:user@example.com">
-            <div className="cta">Send us an email</div>
+          <a href="mailto:astroianu530@gmail.com">
+            <div className="cta">Submit feature request</div>
           </a>
-        </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
